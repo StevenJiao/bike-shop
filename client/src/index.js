2de2c4da0166dfe20b96de889698d40b818146c3
syjiao@ualberta.ca <syjiao@ualberta.ca>
2023-02-10 21:09:51 -0700

    initial login formatted

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import createTheme from '@mui/material/styles/createTheme';
+import { ThemeProvider } from '@emotion/react';
+import { CssBaseline } from '@mui/material';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark'
+  }
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme ={darkTheme}>
+      <CssBaseline></CssBaseline>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
