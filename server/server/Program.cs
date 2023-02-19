e1741c132a8c723fbc42804ad7e65069d22ec022
stevenjiao <syjiao@ualberta.ca>
2023-02-19 14:54:26 -0700

    adding static file support and fallback controller

--- server/server/Program.cs
+++ server/server/Program.cs
@@ -1,5 +1,4 @@
 using Microsoft.Azure.Cosmos;
-using System.Configuration;
 using Microsoft.AspNetCore.Authentication;
 using BasicAuthenticationHandler = server.Handlers.BasicAuthenticationHandler;
 using IUserServices = server.Services.IUserService;
@@ -13,23 +12,33 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// authentication handler
 builder.Services
     .AddAuthentication("BasicAuthentication")
     .AddScheme<AuthenticationSchemeOptions, BasicAuthenticationHandler>("BasicAuthentication", null);
 builder.Services.AddAuthorization();
-
 builder.Services.AddScoped<IUserServices, UserService>();
 
 // The Azure Cosmos DB endpoint for running this sample.
 string? EndpointUri = System.Configuration.ConfigurationManager.AppSettings["EndPointUri"];
 // The primary key for the Azure Cosmos account.
 string? PrimaryKey = System.Configuration.ConfigurationManager.AppSettings["PrimaryKey"];
-
 builder.Services.AddSingleton<CosmosClient>(x => new CosmosClient(EndpointUri, PrimaryKey));
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin().Build();
+    });
+});
+
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
-app.UseCors(options => options.WithOrigins("http://localhost:3000").AllowAnyHeader().AllowAnyMethod());
+//app.UseCors(options => options.WithOrigins("http://localhost:3000").AllowAnyHeader().AllowAnyMethod());
+
+app.UseCors();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -42,8 +51,17 @@ app.UseHttpsRedirection();
 
 app.UseAuthentication();
 
-app.UseAuthorization();
+//app.MapControllers();
 
-app.MapControllers();
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthorization();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+    endpoints.MapFallbackToController("Index", "Fallback");
+});
 
 app.Run();
